041004c48e7a5836814bfb44f0fd7bed88fc069a
Dino A. Navarroli <dino.navarroli@eightbot.com>
2019-04-05 16:41:16 -0500

    Implemented other controls sample screen

---
+++ AuroraBorealis/OtherControls.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace AuroraBorealis
+{
+    public partial class OtherControls : ContentPage
+    {
+        public OtherControls()
+        {
+            InitializeComponent();
+        }
+    }
+}
